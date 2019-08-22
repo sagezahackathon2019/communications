bab09e82df3352b7f6aa9585f112fa22dbd65fde
Marcin Wójcik <marcin.wojcik@sage.com>
2019-08-22 09:26:43 +0200

    Add Vendor Registration

---
+++ Communications/Communications.API/Models/DTOs/VendorDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Communications.API.Models.DTOs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class VendorDto
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string DefaultFromAddress { get; set; }
+        public string Name { get; set; }
+    }
+}
