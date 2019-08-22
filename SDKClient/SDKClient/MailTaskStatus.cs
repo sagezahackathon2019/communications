5fae0cd9aca1f1790ce20f6c1594264f6c31318c
Marcin Wójcik <marcin.wojcik@sage.com>
2019-08-22 16:01:56 +0200

    SDKClient rewrite

---
+++ SDKClient/SDKClient/MailTaskStatus.cs
@@ -0,0 +1,224 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDKClient
+{
+    public class MailTaskStatus
+    {
+        public string BaseUrl { get; set; }
+        public string ReceivedTimestamp { get; set; }
+        public string VendorId { get; set; }
+        public bool Processed { get; set; }
+        public string ProcessedTimestamp { get; set; }
+        public string TaskId { get; set; }
+        public int ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+        public int Success { get; set; }
+        public int RecordIndex = -1;
+
+        internal List<MailTaskStatusDTO> MailTaskList { get; set; }
+
+        internal class SingleResponse
+        {
+            internal MailTaskStatusDTO Payload { get; set; }
+            internal bool Success { get; set; }
+        }
+
+        internal class ListResponse
+        {
+            internal List<MailTaskStatusDTO> Payload { get; set; }
+            internal bool Success { get; set; }
+        }
+
+        internal class MailTaskStatusDTO
+        {
+            public string ReceivedTimestamp { get; set; }
+            public string VendorId { get; set; }
+            public bool Processed { get; set; }
+            public string ProcessedTimestamp { get; set; }
+            public string TaskId { get; set; }
+        }
+
+        public void GetEmailTaskStatus(string taskId, string vendorKey)
+        {
+            RecordIndex = -1;
+            try
+            {
+                string result = WebUtility.SendRequestJSON($"{BaseUrl}/api/communications/mail/status/{taskId}", "GET", vendorKey, null, null);
+                var response = JsonConvert.DeserializeObject<SingleResponse>(result);
+                Success = response.Success ? 1 : 0;
+
+                if (!response.Success)
+                {
+                    return;
+                }
+
+                var mailTaskResult = response.Payload;
+                Processed = mailTaskResult.Processed;
+                ProcessedTimestamp = mailTaskResult.ProcessedTimestamp;
+                ReceivedTimestamp = mailTaskResult.ReceivedTimestamp;
+                TaskId = mailTaskResult.TaskId;
+                VendorId = mailTaskResult.VendorId;
+
+                return;
+            }
+            catch (Exception exc)
+            {
+                Success = 0;
+                ErrorCode = -1;
+                ErrorMessage = exc.Message;
+            }
+
+            return;
+        }
+
+        public void NextRecord()
+        {
+            if (RecordIndex > -1)
+                RecordIndex++;
+
+            if (RecordIndex < MailTaskList.Count() && RecordIndex > -1)
+            {
+                var mailTaskResult = MailTaskList[RecordIndex];
+                Processed = mailTaskResult.Processed;
+                ProcessedTimestamp = mailTaskResult.ProcessedTimestamp;
+                ReceivedTimestamp = mailTaskResult.ReceivedTimestamp;
+                TaskId = mailTaskResult.TaskId;
+                VendorId = mailTaskResult.VendorId;
+            }
+
+            if (RecordIndex >= MailTaskList.Count())
+                RecordIndex = -1;
+        }
+
+        public void GetAllEmailTasksForClient(string vendorKey, string clientId)
+        {
+            try
+            {
+                string result = WebUtility.SendRequestJSON($"{BaseUrl}/api/communications/mail/status/all?vendorId={vendorKey}&clientId={clientId}", "GET", vendorKey, null, null);
+                var response = JsonConvert.DeserializeObject<ListResponse>(result);
+                Success = response.Success ? 1 : 0;
+
+                if (!response.Success)
+                {
+                    return;
+                }
+
+                var mailTaskResult = response.Payload;
+                if (mailTaskResult.Count == 0)
+                {
+                    RecordIndex = -1;
+                    return;
+                }
+
+                MailTaskList = mailTaskResult;
+                RecordIndex = 0;
+                var mailTask = MailTaskList[RecordIndex];
+                Processed = mailTask.Processed;
+                ProcessedTimestamp = mailTask.ProcessedTimestamp;
+                ReceivedTimestamp = mailTask.ReceivedTimestamp;
+                TaskId = mailTask.TaskId;
+                VendorId = mailTask.VendorId;
+
+                return;
+            }
+            catch (Exception exc)
+            {
+                RecordIndex = -1;
+                Success = 0;
+                ErrorCode = -1;
+                ErrorMessage = exc.Message;
+            }
+
+            return;
+        }
+
+        public void GetUnprocessedEmailTasksForClient(string vendorKey, string clientId)
+        {
+            try
+            {
+                string result = WebUtility.SendRequestJSON($"{BaseUrl}/api/communications/mail/status/unprocessed?vendorId={vendorKey}&clientId={clientId}", "GET", vendorKey, null, null);
+                var response = JsonConvert.DeserializeObject<ListResponse>(result);
+                Success = response.Success ? 1 : 0;
+
+                if (!response.Success)
+                {
+                    return;
+                }
+
+                var mailTaskResult = response.Payload;
+                if (mailTaskResult.Count == 0)
+                {
+                    RecordIndex = -1;
+                    return;
+                }
+
+                MailTaskList = mailTaskResult;
+                RecordIndex = 0;
+                var mailTask = MailTaskList[RecordIndex];
+                Processed = mailTask.Processed;
+                ProcessedTimestamp = mailTask.ProcessedTimestamp;
+                ReceivedTimestamp = mailTask.ReceivedTimestamp;
+                TaskId = mailTask.TaskId;
+                VendorId = mailTask.VendorId;
+
+                return;
+            }
+            catch (Exception exc)
+            {
+                RecordIndex = -1;
+                Success = 0;
+                ErrorCode = -1;
+                ErrorMessage = exc.Message;
+            }
+
+            return;
+        }
+
+        public void GetProcessedEmailTasksForClient(string vendorKey, string clientId)
+        {
+            try
+            {
+                string result = WebUtility.SendRequestJSON($"{BaseUrl}/api/communications/mail/status/processed?vendorId={vendorKey}&clientId={clientId}", "GET", vendorKey, null, null);
+                var response = JsonConvert.DeserializeObject<ListResponse>(result);
+                Success = response.Success ? 1 : 0;
+
+                if (!response.Success)
+                {
+                    return;
+                }
+
+                var mailTaskResult = response.Payload;
+                if (mailTaskResult.Count == 0)
+                {
+                    RecordIndex = -1;
+                    return;
+                }
+
+                MailTaskList = mailTaskResult;
+                RecordIndex = 0;
+                var mailTask = MailTaskList[RecordIndex];
+                Processed = mailTask.Processed;
+                ProcessedTimestamp = mailTask.ProcessedTimestamp;
+                ReceivedTimestamp = mailTask.ReceivedTimestamp;
+                TaskId = mailTask.TaskId;
+                VendorId = mailTask.VendorId;
+
+                return;
+            }
+            catch (Exception exc)
+            {
+                RecordIndex = -1;
+                Success = 0;
+                ErrorCode = -1;
+                ErrorMessage = exc.Message;
+            }
+
+            return;
+        }
+    }
+}
---
+++ SDKClient/SDKClient/MailTaskSubmitter.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDKClient
+{
+    public class MailTaskSubmitter
+    {
+        public string BaseUrl { get; set; }
+        public string ReceivedTimestamp { get; set; }
+        public bool Processed { get; set; }
+        public string StatusUrl { get; set; }
+        public string CancelUrl { get; set; }
+        public string TaskId { get; set; }
+        public string VendorId { get; set; }
+        public int ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+        public int Success { get; set; }
+
+        internal class Response
+        {
+            internal MailTaskResult Payload { get; set; }
+            internal bool Success { get; set; }
+        }
+
+        internal class MailTaskResult
+        {
+            internal string ReceivedTimestamp { get; set; }
+            internal bool Processed { get; set; }
+            internal string StatusUrl { get; set; }
+            internal string CancelUrl { get; set; }
+            internal string TaskId { get; set; }
+            internal string VendorId { get; set; }
+        }
+
+        public void SendEmailTask(string vendorKey, string clientId, string from, string to, string subject, string body)
+        {
+            try
+            {
+                MailObject mailObject = new MailObject
+                {
+                    Body = body,
+                    ClientId = clientId,
+                    From = from,
+                    Subject = subject,
+                    To = to.Split(';')
+                };
+
+                var result = WebUtility.SendRequestJSON($"{BaseUrl}/api/communications/mail/submit", "POST", vendorKey, mailObject, null);
+                var response = JsonConvert.DeserializeObject<Response>(result);
+
+                Success = response.Success ? 1 : 0;
+
+                if (!response.Success)
+                {
+                    return;
+                }
+
+                var mailTaskResult = response.Payload;
+
+                ReceivedTimestamp = mailTaskResult.ReceivedTimestamp;
+                Processed = mailTaskResult.Processed;
+                CancelUrl = mailTaskResult.CancelUrl;
+                StatusUrl = mailTaskResult.StatusUrl;
+                TaskId = mailTaskResult.TaskId;
+                VendorId = mailTaskResult.VendorId;
+                return;
+            }
+            catch (Exception exc)
+            {
+                Success = 0;
+                ErrorCode = -1;
+                ErrorMessage = exc.Message;
+            }
+
+            return;
+        }
+    }
+}
