f5b39741a31de9b71c093cf84b1e508ddd8c554b
kairynners <kai.rynners@sage.com>
2019-08-22 09:11:39 +0200

    added MainDbContext (no migrations) and some folder structure for the models

---
+++ Communications/Communications.API/Models/Domain/MailTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Communications.API.Models.Domain
+{
+    public class MailTask
+    {
+        public Guid Id { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool Processed { get; set; }
+        public DateTime? ProcessedTimestamp { get; set; }
+        public DateTime ReceivedTimestamp { get; set; }
+        public Guid VendorId { get; set; }
+        public string ClientId { get; set; }
+    }
+}
---
+++ Communications/Communications.API/Models/ResponseObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Communications.API.Models
+{
+    public class ResponseObject<T>
+    {
+        public T Payload { get; set; }
+        public bool Success { get; set; }
+    }
+}
